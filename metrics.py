b47f73b6ed57971fd79a148c3a2a93a97e271666
Rodney <r.guthman@gmail.com>
2021-04-19 20:22:18 +0200

    Added various basic image metrics

--- metrics.py
+++ metrics.py
@@ -2,34 +2,52 @@ import cv2
 import skimage.measure
 import imquality.brisque as brisque
 import webcolors as wc
+from skimage.filters import sobel
+from pybdm import BDM
 import pylab as pl
 import numpy as np
 import sys
 
-# The cpbd module tries to import imread from scipy.ndimage, where it does not exist. This assignment overrides that.
+# The cpbd module tries to import scipy.ndimage.imread, which does not exist. This line below is a fix for this issue.
 # noinspection PyTypeChecker
 sys.modules['scipy.ndimage.imread'] = cv2.imread
 import cpbd
 
 # TODO: check for RGB/BGR inconsistencies
 # TODO: validate metrics (is the implementation correct?)
+# TODO: check correlation between k_complexity_bw() and k_complexity_lightness() (better yet, check all correlations)
+# TODO: wrt to the k_complexity functions, is the metric of 2D array sufficiently correlated with its 1D version?
 
 
-def mean_pixel_intensity(image):
+def contrast_rms(image):
+    # Returns RMS contrast
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return image.std()
+
+
+# noinspection PyTypeChecker
+def contrast_tenengrad(image):
+    # Returns tenengrad contrast
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    sobel_img = sobel(image) ** 2
+    return np.sqrt(np.sum(sobel_img)) / image.size
+
+
+def pixel_intensity_mean(image):
     return image.mean()
 
 
-def mean_hue(image):
+def hue_mean(image):
     return cv2.cvtColor(image, cv2.COLOR_BGR2HSV).mean()
 
 
-def mean_saturation(image):
+def saturation_mean(image):
     img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
     saturation = img_hsv[:, :, 1].mean()
     return saturation
 
 
-def shannon_entropy(image):
+def entropy_shannon(image):
     return skimage.measure.shannon_entropy(image)
 
 
@@ -70,11 +88,16 @@ def sharpness(image):
     return cpbd.compute(image)
 
 
+def sharpness_laplacian(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return cv2.Laplacian(image, cv2.CV_64F).var()
+
+
 def brisque_score(image):
     return brisque.score(image)
 
 
-def dominant_color(image):
+def color_dominant(image):
 
     def get_approx_color(hex_color):
         orig = wc.hex_to_rgb(hex_color)
@@ -100,7 +123,7 @@ def dominant_color(image):
     compactness, labels, centers = cv2.kmeans(data, 1, None, criteria, 10, flags)
     bgr_ = centers[0].astype(np.int32)
     rgb_ = bgr_
-    rgb_[0], rgb_[2] = bgr_[2], bgr_[0] # convert BGR to RGB
+    rgb_[0], rgb_[2] = bgr_[2], bgr_[0]  # convert BGR to RGB
     hex_ = wc.rgb_to_hex(tuple(rgb_))
 
     return get_color_name(hex_)
@@ -118,3 +141,46 @@ def colorfulness(image):
     std_root = np.sqrt((rbStd ** 2) + (ybStd ** 2))
     mean_root = np.sqrt((rbMean ** 2) + (ybMean ** 2))
     return std_root + (0.3 * mean_root)
+
+
+def k_complexity_bw(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = image.reshape(image.shape[0]*image.shape[1])
+    image = image*(252/256)
+    bins = list(range(0, 252, 28))
+    image = np.digitize(image, bins=bins)-1
+    bdm = BDM(ndim=1, nsymbols=9, warn_if_missing_ctm=False)
+    return bdm.bdm(image)
+
+
+def k_complexity_lab_l(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+    image = image.astype('float32') / 255  # transformation to fix Euclidian distances in Lab space
+    image = image[:, :, 0]
+    image = image.reshape(image.shape[0]*image.shape[1])
+    bins = list(np.arange(0, 0.9, 0.1))
+    image = np.digitize(image, bins=bins)-1
+    bdm = BDM(ndim=1, nsymbols=9, warn_if_missing_ctm=False)
+    return bdm.bdm(image)
+
+
+def k_complexity_lab_a(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+    image = image.astype('float32') / 255  # transformation to fix Euclidian distances in Lab space
+    image = image[:, :, 1]
+    image = image.reshape(image.shape[0]*image.shape[1])
+    bins = list(np.arange(0, 0.9, 0.1))
+    image = np.digitize(image, bins=bins)-1
+    bdm = BDM(ndim=1, nsymbols=9, warn_if_missing_ctm=False)
+    return bdm.bdm(image)
+
+
+def k_complexity_lab_b(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2Lab)
+    image = image.astype('float32') / 255  # transformation to fix Euclidian distances in Lab space
+    image = image[:, :, 2]
+    image = image.reshape(image.shape[0]*image.shape[1])
+    bins = list(np.arange(0, 0.9, 0.1))
+    image = np.digitize(image, bins=bins)-1
+    bdm = BDM(ndim=1, nsymbols=9, warn_if_missing_ctm=False)
+    return bdm.bdm(image)
